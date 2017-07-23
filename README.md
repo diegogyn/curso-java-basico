be2cf945b366ad3f6023b23ef73ca68c1496a567
Diego <diegogyn@users.noreply.github.com>
2017-07-22 22:40:59 -0300

    atualização
    Nova aula leitura de dados

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # inicio 18/07/2017
 Curso de Java Basico
 
-
 Aulas:
 http://loiane.training/
 
+IDE Eclipse Oxygen
--- src/com/diego/cursojava/aula12/LeituraDados.java
+++ src/com/diego/cursojava/aula12/LeituraDados.java
@@ -6,10 +6,11 @@ public class LeituraDados {
 
 	public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in)
+		Scanner scan = new Scanner(System.in);
 				
 		System.out.println("Digite seu nome Completo:");
 		String nomeCompleto = scan.nextLine();
+		System.out.println("O nome que você digitou foi: " + nomeCompleto);
 	}
 
 }
